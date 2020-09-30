f9d288ddf5b096a7fb1f6123bbdb90ebf2c0300c
nviable <saniat.s@mail.rit.edu>
2020-09-30 15:49:08 -0400

    fixed undefined window

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,12 +1,19 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import "../styles/PagesLayout.scss"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 import SEO from "../seo"
 
 const PageLayout = ({children}) => {
-    const pathname = window.location.pathname.split("/")[1]
-    const className = (pathname == "") ? 'page-home' : pathname
+    const [ pathname, setPathname ] = useState("")
+    const [ className, setClassName ] = useState("")
+    
+    useEffect( () => {
+        if (window) {
+            setPathname(window.location.pathname.split("/")[1])
+            setClassName((pathname == "") ? 'page-home' : pathname)
+        }
+    })
 
     return(<div>
             <Header />
