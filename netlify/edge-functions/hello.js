0b54408b33bcd4e1ac275d5e70587d61e9c12a2a
John Sohrawardi <saniat.s@gmail.com>
2022-12-11 15:25:51 -0500

    rebuild within tutorial

---
+++ netlify/edge-functions/hello.js
@@ -0,0 +1,8 @@
+export default async (request, context) => {
+    return new Response("Hello World from the edge!",
+    {
+        headers: {
+            "content-type": "text/html"
+        },
+    });
+};
